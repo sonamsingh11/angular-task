35b6ffa332930d77a33eb96413a6e9f9641d4540
Kao-Wei Chen <kao@flipgrid.com>
2019-12-11 23:56:34 -0800

    add homepage navigation button

--- src/app/features/home-page/home-page.component.ts
+++ src/app/features/home-page/home-page.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
     selector: 'app-home-page',
     styleUrls: ['./home-page.component.less'],
     templateUrl: './home-page.component.html'
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+    constructor(private router: Router) {
+    }
+
+    goToListPage() {
+        this.router.navigate(['profile-list']);
+    }
+}
